e3f1ff3a265db76b7f6f0f6e2b6ee6e8403053f2
Dhruv Fadadu <dhruvfadadu11@gmail.com>
2024-03-30 13:46:22 +0000

    c

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,11 +1,7 @@
 package com.example.controller;
 
-
 import java.util.List;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.h2.util.json.JSONObject;
-import org.h2.util.json.JSONValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,7 +1,5 @@
 package com.example.service;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

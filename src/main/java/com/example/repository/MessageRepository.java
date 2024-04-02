c2cc7c952374a636462d74e3343aee00a3631f8c
Dhruv Fadadu <dhruvfadadu11@gmail.com>
2024-04-02 07:14:51 +0000

    final

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
-@Repository
+
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
     @Query("FROM Message m WHERE m.posted_by = :account_id")
